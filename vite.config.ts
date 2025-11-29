6bb9f14fce4d5a546d186392c86edb1f667b114a
anvar <fort2652@rambler.ru>
2025-11-29 12:11:31 +0300

    main: add base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      // Provide an explicit app-level constant derived from an env var.
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
-  },
+    base: `${env.APP_BASE_URL}`,
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
 });
